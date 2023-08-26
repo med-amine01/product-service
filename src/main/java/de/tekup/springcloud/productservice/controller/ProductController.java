4b69807457107c12646adf2dc9202fe8e9fe5332
med-amine01 <MohamedAmine.CHEBBI@tek-up.de>
2023-08-26 11:39:54 +0100

    adding business logic

--- src/main/java/de/tekup/springcloud/productservice/controller/ProductController.java
+++ src/main/java/de/tekup/springcloud/productservice/controller/ProductController.java
@@ -1,57 +1,95 @@
 package de.tekup.springcloud.productservice.controller;
 
-import de.tekup.springcloud.productservice.entity.Product;
-import de.tekup.springcloud.productservice.service.serviceImpl.ProductService;
+import de.tekup.springcloud.productservice.dto.APIResponse;
+import de.tekup.springcloud.productservice.dto.ProductRequestDTO;
+import de.tekup.springcloud.productservice.dto.ProductResponseDTO;
+import de.tekup.springcloud.productservice.service.ProductServiceInterface;
+import de.tekup.springcloud.productservice.util.ValueMapper;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1/products")
 @AllArgsConstructor
+@RequestMapping("/api/v1/products")
+@Slf4j
 public class ProductController {
-
-    private final ProductService productService;
-
+    public static final String SUCCESS = "SUCCESS";
+    private final ProductServiceInterface productServiceInterface;
+    
     @GetMapping
-    public List<Product> getAllProducts() {
-        return productService.getAllProducts();
+    public ResponseEntity<APIResponse<List<ProductResponseDTO>>> getAllProducts() {
+        List<ProductResponseDTO> products = productServiceInterface.getProducts();
+        
+        APIResponse<List<ProductResponseDTO>> responseDTO = APIResponse
+                .<List<ProductResponseDTO>>builder()
+                .status(SUCCESS)
+                .results(products)
+                .build();
+        
+        log.info("ProductController::getAllProducts response {}", ValueMapper.jsonToString(responseDTO));
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
-
+    
     @GetMapping("/{id}")
-    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
-        Optional<Product> optionalProduct = productService.getProductById(id);
-
-        return optionalProduct.map(product -> new ResponseEntity<>(product, HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    public ResponseEntity<APIResponse<ProductResponseDTO>> getProductById(@PathVariable Long id) {
+        log.info("ProductController::getProductById {}", id);
+        ProductResponseDTO productResponseDTO = productServiceInterface.getProductById(id);
+        
+        APIResponse<ProductResponseDTO> responseDTO = APIResponse
+                .<ProductResponseDTO>builder()
+                .status(SUCCESS)
+                .results(productResponseDTO)
+                .build();
+        
+        log.info("ProductController::getProductById {} response {}", id, ValueMapper.jsonToString(productResponseDTO));
+        
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
-
+    
     @PostMapping
-    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
-        Product createdProduct = productService.createProduct(product);
-        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
+    public ResponseEntity<APIResponse<ProductResponseDTO>> createProduct(@RequestBody @Valid ProductRequestDTO productRequestDTO) {
+        log.info("ProductController::createProduct request body {}", ValueMapper.jsonToString(productRequestDTO));
+        
+        ProductResponseDTO createdProduct = productServiceInterface.createProduct(productRequestDTO);
+        
+        APIResponse<ProductResponseDTO> responseDTO = APIResponse
+                .<ProductResponseDTO>builder()
+                .status(SUCCESS)
+                .results(createdProduct)
+                .build();
+        
+        log.info("ProductController::createProduct response {}", ValueMapper.jsonToString(responseDTO));
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
     }
-
+    
     @PutMapping("/{id}")
-    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product product) {
-        Product updatedProduct = productService.updateProduct(id, product);
-
-        return updatedProduct != null ? new ResponseEntity<>(updatedProduct, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<APIResponse<ProductResponseDTO>> updateProduct(
+            @PathVariable Long id,
+            @Valid @RequestBody ProductRequestDTO productRequestDTO
+    ) {
+        log.info("ProductController::updateProduct request body {}", ValueMapper.jsonToString(productRequestDTO));
+        
+        ProductResponseDTO updatedProduct = productServiceInterface.updateProduct(id, productRequestDTO);
+        
+        APIResponse<ProductResponseDTO> responseDTO = APIResponse
+                .<ProductResponseDTO>builder()
+                .status(SUCCESS)
+                .results(updatedProduct)
+                .build();
+        
+        log.info("ProductController::updateProduct response {}", ValueMapper.jsonToString(responseDTO));
+        return ResponseEntity.ok(responseDTO);
     }
-
+    
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
-        productService.deleteProduct(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        productServiceInterface.deleteProduct(id);
+        return ResponseEntity.noContent().build();
     }
 }
